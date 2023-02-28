5ffb66eb6afada4360e887c0bf7780917cf57d64
UnknownUser95 <luluklzde@gmail.com>
2023-02-28 10:24:25 +0100

    changer server, add Serial example

---
+++ SerialCommunication/SerialCommunication.ino
@@ -0,0 +1,22 @@
+#include <HardwareSerial.h>
+
+HardwareSerial &hSerial = Serial2; //can be Serial2 as well, just use proper pins
+
+void setup() 
+{
+  Serial.begin(115200);//open serial via USB to PC on default port
+  hSerial.begin(115200);//open the other serial port
+}
+
+void loop() 
+{  
+  if (Serial.available()) //check incoming on default serial (USB) from PC
+      { 
+        hSerial.write(Serial.read());   // read it from UBS and send it to hSerial 
+      }
+
+  if (hSerial.available()) //check incoming on other serial from the other board
+      { 
+        Serial.write(hSerial.read());   //read it from hSerial and send it to  UBS
+      }
+}
