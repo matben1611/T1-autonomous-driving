f1de3cf559958111074e01615c054980792907f3
UnknownUser95 <luluklzde@gmail.com>
2023-03-16 20:27:41 +0100

    change website and API design

--- T1/T1.ino
+++ T1/T1.ino
@@ -2,11 +2,12 @@
 #include <WebServer.h>
 #include <WiFi.h>
 
+#define HTTP_OKAY 200
+#define HTTP_USER_ERROR 400
+#define HTTP_NOT_FOUND 404
+
 #define TYPE_TEXT "text/plain"
 #define TYPE_HTML "text/html"
-
-// 301 as redirect response must not be used due to caching
-#define REDIRECT 302
 
 #define FRONT_LED 13
 #define BACK_LED 15
@@ -14,14 +15,14 @@ bool FRONT_LED_STATUS = LOW;
 bool BACK_LED_STATUS = LOW;
 
 #define DEFAULT_STEERING_ANGLE 90 // 0 - 180: 90 is straight
-#define SERVO_PIN 14
+#define SERVO_PIN 32
 Servo servo;
 
 #define MOTOR_PIN_1 16
 #define MOTOR_PIN_2 17
-#define MOTOR_PIN_1_CHANNEL 1
-#define MOTOR_PIN_2_CHANNEL 2
-#define MOTOR_FREQUENCY 1000
+#define MOTOR_PIN_1_CHANNEL 14
+#define MOTOR_PIN_2_CHANNEL 15
+#define MOTOR_FREQUENCY 50
 #define MOTOR_RESOLUTION 8
 #define MAX_PWM_VALUE ((1 << MOTOR_RESOLUTION) - 1)
 
@@ -58,8 +59,8 @@ void setup() {
 	server.on("/", handle_root);
 	server.on("/togglefront", handle_toggle_front);
 	server.on("/toggleback", handle_toggle_back);
-	server.on("/steering_dir", handle_steering);
-	server.on("/speed", handle_speed);
+	server.on("/steering", handle_steering);
+	server.on("/motor", handle_speed);
 	server.onNotFound(handle_NotFound);
 
 	server.begin();
@@ -78,9 +79,13 @@ int normalize(int input) {
 	return input;
 }
 
+String printBool(bool b) {
+	return b ? "true" : "false";
+}
+
 void handle_speed() {
-	if(server.hasArg("value")) {
-		int speed = server.arg("value").toInt();
+	if(server.hasArg("speed")) {
+		int speed = server.arg("speed").toInt();
 		speed = normalize(speed);
 		Serial.print("setting speed to ");
 		Serial.println(speed);
@@ -94,13 +99,12 @@ void handle_speed() {
 			backward = abs(speed);
 		}
 		
-		ledcWrite(MOTOR_PIN_1_CHANNEL, 0);
-		ledcWrite(MOTOR_PIN_2_CHANNEL, 0);
+		ledcWrite(MOTOR_PIN_1_CHANNEL, forward);
+		ledcWrite(MOTOR_PIN_2_CHANNEL, backward);
 		
-		ledcWrite(MOTOR_PIN_1_CHANNEL, speed);
-		server.send(200, TYPE_TEXT, String(speed));
+		server.send(HTTP_OKAY, TYPE_TEXT, String(speed));
 	} else {
-		server.send(400, TYPE_TEXT, "param 'value' missing");
+		server.send(HTTP_USER_ERROR, TYPE_TEXT, "param 'speed' missing");
 	}
 }
 
@@ -110,9 +114,9 @@ void handle_steering() {
 		Serial.print("setting angle to ");
 		Serial.println(angle);
 		servo.write(angle);
-		server.send(200, TYPE_TEXT, String(angle));
+		server.send(HTTP_OKAY, TYPE_TEXT, String(angle));
 	} else {
-		server.send(400, TYPE_TEXT, "param 'angle' missing");
+		server.send(HTTP_USER_ERROR, TYPE_TEXT, "param 'angle' missing");
 	}
 }
 
@@ -123,65 +127,68 @@ void loop() {
 }
 
 void handle_root() {
-	server.send(200, TYPE_HTML, sendHTML());
+	server.send(HTTP_OKAY, TYPE_HTML, sendHTML());
 }
 
 void handle_toggle_front() {
 	FRONT_LED_STATUS = !FRONT_LED_STATUS;
 	Serial.print("front: ");
 	Serial.println(FRONT_LED_STATUS);
-	server.sendHeader("location", "/", true);
-	server.send(REDIRECT, TYPE_HTML, "");
+	server.send(HTTP_OKAY, TYPE_TEXT, printBool(FRONT_LED_STATUS));
 }
 
 void handle_toggle_back() {
 	BACK_LED_STATUS = !BACK_LED_STATUS;
 	Serial.print("back: ");
 	Serial.println(BACK_LED_STATUS);
-	server.sendHeader("location", "/", true);
-	server.send(REDIRECT, TYPE_HTML, "");
+	server.send(HTTP_OKAY, TYPE_TEXT, printBool(BACK_LED_STATUS));
 }
 
 void handle_NotFound() {
-	server.send(404, TYPE_TEXT, "Not found");
+	server.send(HTTP_NOT_FOUND, TYPE_TEXT, "Not found");
 }
 
 String sendHTML() {
 	  String ptr = \
-"<!DOCTYPE html> <html>\n\
-<head><meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>\n\
-	<title>ESP32 T1</title>\n\
+"<!DOCTYPE html> <html>\
+<head><meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>\
+	<title>ESP32 T1</title>\
 	<style>\
 		html { display: inline-block; margin: 0px auto; text-align: center;}\n\
 		body {margin-top: 50px;} \
 		h1 {color: #444444;margin: 50px auto 30px;}\
 		h3 {color: #444444;margin-bottom: 50px;}\
-		.button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n\
-		.button-on {background-color: #3498db;}\n\
-		.button-on:active {background-color: #2980b9;}\n\
-		.button-off {background-color: #34495e;}\n\
-		.button-off:active {background-color: #2c3e50;}\n\
-		p {font-size: 14px;color: #888;margin-bottom: 10px;}\n\
-	</style>\n\
-</head>\n\
-<body>\n\
-	<h1>ESP32 T1 control</h1>\n\
-	<h3>AP Mode</h3>\n";
-
-	if (FRONT_LED_STATUS) {
-		ptr += "<p>front LED: ON</p><a class='button button-off' href='/togglefront'>OFF</a>\n";
-	} else {
-		ptr += "<p>front LED: OFF</p><a class='button button-on' href='/togglefront'>ON</a>\n";
-	}
-
-	if (BACK_LED_STATUS) {
-	ptr += "<p>back LED: ON</p><a class='button button-off' href='/toggleback'>OFF</a>\n";
-
-	} else {
-		ptr += "<p>back LED: OFF</p><a class='button button-on' href='/toggleback'>ON</a>\n";
-	}
-
-	ptr += "</body>\n</html>\n";
+		.button {width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;}\
+		.button-on {background-color: #3498db;}\
+		.button-on:active {background-color: #2980b9;}\
+		.button-off {background-color: #34495e;}\
+		.button-off:active {background-color: #2c3e50;}\
+		div {margin: 40px;}\
+	</style>\
+	<script>function getRequest(to, target){let req = new XMLHttpRequest();req.open('GET',to,false);req.send(null);if(target!=null){document.getElementById(target).innerHTML=req.responseText;} }</script>\
+</head>\
+<body>\
+	<h1>ESP32 T1 control</h1>\
+	<h3>AP Mode</h3>\
+	<div>\
+		<span class='button' onclick='getRequest(\"/togglefront\", \"lights-front\")'>front</span>\
+		<span id='lights-front'>false</span>\
+	</div>\
+	<div>\
+		<span class='button' onclick='getRequest(\"/toggleback\", \"lights-back\")'>back</span>\
+		<span id='lights-back'>false</span>\
+	<div>\
+	<div><p id='steer'>90</p>\
+		<span class='button' onclick='getRequest(\"/steering?angle=-90\", \"steer\")'>left</span>\
+		<span class='button' onclick='getRequest(\"/steering?angle=0\", \"steer\")'>straight</span>\
+		<span class='button' onclick='getRequest(\"/steering?angle=90\", \"steer\")'>right</span>\
+	</div>\
+	<div><p id='speed'>0</p>\
+		<span class='button' onclick='getRequest(\"/motor?speed=255\", \"speed\")'>forward</span>\
+		<span class='button' onclick='getRequest(\"/motor?speed=0\", \"speed\")'>stop</span>\
+		<span class='button' onclick='getRequest(\"/motor?speed=-255\", \"speed\")'>backward</span>\
+	</div>\
+</body>\n</html>\n";
 
 	return ptr;
 }
