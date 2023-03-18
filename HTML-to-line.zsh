c4e868e71de09d7d737635ba79a2d623a09cb03d
UnknownUser95 <luluklzde@gmail.com>
2023-03-18 13:44:59 +0100

    better compressor

--- HTML-to-line.zsh
+++ HTML-to-line.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-result=`cat site-design.html |  tr -d '\n' | tr -d '\t'`
+result=`cat site-design.html | tr -d '\n' | tr -d '\t' | sed 's/\"/\\\"/g'`
 echo $result
 echo $result > compressed-site.html
 echo $result | xclip -sel clip
